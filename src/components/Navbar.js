9c309b104e5425fb67598086434b461b5e830819
galihprad <galih.p.w.j@gmail.com>
2020-11-07 21:13:33 +0700

    Add Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="Navbar">
+      <div className="navItem">Domain</div>
+      <div className="navItem">Hosting</div>
+      <div className="navItem">Sign In</div>
+    </div>
+  );
+}
