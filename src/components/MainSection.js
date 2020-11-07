603b3171118ce453913142dc17fd4d4f1cb9973b
galihprad <galih.p.w.j@gmail.com>
2020-11-07 21:13:06 +0700

    Add MainSection component

---
+++ src/components/MainSection.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function MainSection() {
+  return (
+    <div className="sectionStyle">
+      <div className="header1">Layanan Hosting Terbaik</div>
+      <div className="header2">Domainku</div>
+    </div>
+  );
+}
