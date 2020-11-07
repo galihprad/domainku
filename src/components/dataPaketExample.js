968adb71ffb1c9664f4c6d72eb5a5b9a75630cae
galihprad <galih.p.w.j@gmail.com>
2020-11-07 21:12:27 +0700

    Add example data paket

---
+++ src/components/dataPaketExample.js
@@ -0,0 +1,39 @@
+export const dataPaketExample = [
+  {
+    title: "Paket A",
+    price: "16k",
+    feature: [
+      "750 MB Space",
+      "Unlimited Bandwidth",
+      "5 Website",
+      "Unlimited Email",
+      "Supports IPv4 & IPv6",
+    ],
+  },
+  {
+    title: "Paket B",
+    price: "32k",
+    feature: [
+      "2 GB Space",
+      "Unlimited Bandwidth",
+      "Unlimited Website",
+      "Unlimited Email",
+      "Supports IPv4 & IPv6",
+      "Gratis Domain! ",
+      "Akses SSH",
+    ],
+  },
+  {
+    title: "Paket C",
+    price: "64k",
+    feature: [
+      "5 GB Space",
+      "Unlimited Bandwidth",
+      "Unlimited Website",
+      "Unlimited Email",
+      "Supports IPv4 & IPv6",
+      "Gratis Domain!",
+      "Akses SSH",
+    ],
+  },
+];
