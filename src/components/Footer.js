153291bbb006cf9aec079b4f860721da7f8fec7b
galihprad <galih.p.w.j@gmail.com>
2020-11-07 21:14:13 +0700

    Add Footer components

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <div className="contactTitle">Kontak</div>
+      <div>PT Deneva</div>
+      <div>Jl. C. Simanjuntak No.65 Yogyakarta 55223</div>
+      <div>D. I. Yogyakarta, Indonesia</div>
+    </div>
+  );
+}
